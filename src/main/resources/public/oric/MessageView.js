eb6f628c2ef488df7f0ba068cd73a4f6bfeb5240
Paul Drummond <paul.drummond@iode.co.uk>
2015-04-23 22:16:18 +0100

    Slide-in sidebar mockup

--- src/main/resources/public/oric/MessageView.js
+++ src/main/resources/public/oric/MessageView.js
@@ -12,6 +12,11 @@ module.exports = Backbone.View.extend({
 	
 	template: _.template($('#MessageViewTemplate').html()),
 	
+	events: {
+		'click': 'onItemClicked',
+		'click #show-detail-button': 'onShowDetailButtonClicked'
+	},
+	
 	initialize: function(options) {
 		this.message = options.message;
 	},
@@ -21,5 +26,20 @@ module.exports = Backbone.View.extend({
 		this.$("#message-content").html(this.model.get('content'));
 		//this.$("#item-image").html(this.generateUserImage());
 		return this.el;
+	},
+	
+	onItemClicked: function() {
+		$("#MessageView a").removeClass('active');
+		this.$("a").addClass("active");		
+	},
+	
+	onShowDetailButtonClicked: function() {
+		if($("#app-container").css('left') == '0px') {
+			$("#app-container").animate({left: '50%'}, 100);
+			$("#left-sidebar").animate({left: '0%'}, 100);
+		} else {
+			$("#app-container").animate({left: '0%'}, 100);
+			$("#left-sidebar").animate({left: '-60%'}, 100);
+		}
 	}
 });
